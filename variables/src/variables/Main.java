57606433685aa6b3afc792901abaf3f9e4561e45
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-09-22 22:23:32 +0300

    Değişken tanımla tamamlandı

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 11;
+		String mesaj ="Öğrenci Sayısı";
+		System.out.println(mesaj+" "+ sayi1);
+	}
+
+}
