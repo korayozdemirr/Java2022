7b2d18e0ff0ed3f328e95a5502b9848537ad41bb
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-09-22 22:30:52 +0300

    datatypes dersi tamamlandı

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		char karakter = 'a';
+		String sehir = "Ankara";
+		boolean dogrumu = false;
+	}
+
+}
