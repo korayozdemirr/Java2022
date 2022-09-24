ef778234f5af78803a9ba0cac404f73ec854858a
KORAY ÖZDEMİR <33378626+korayozdemirr@users.noreply.github.com>
2022-09-24 12:12:34 +0300

    if else şart bloğu öğrenildi

---
+++ Conditionals/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 30;
+		
+		if(sayi<20){
+			System.out.println("Sayı 20den küçük");
+		}else if(sayi == 20){
+			System.out.println("Sayı 20ye eşittir");
+		}else {
+			System.out.println("Sayı 20den büyüktür");
+		}
+
+	}
+
+}
